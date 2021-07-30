c4f2885d61a27117485638959d69272a931a2035
Zechariah Tan <zechariahtan144@gmail.com>
2021-07-31 00:12:56 +0800

    Version 1.3.9 released

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ const IO = new Server(server, {
 	pingInterval: 300000
 })
 const PORT = 5190
-const VERSION = "1.3.8"
+const VERSION = "1.3.9"
 ffmpeg.setFfmpegPath(require("@ffmpeg-installer/ffmpeg").path)
 admin.initializeApp({
 	credential: admin.credential.cert(require("../config.json").firebase.service_account)
